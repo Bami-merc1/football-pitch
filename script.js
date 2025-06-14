b81cc23269e4a5c5eb0fe083ee03c8804703b193
Bamidele Mercy Enoch <mercyenochs06@gmail.com>
2025-06-14 11:19:49 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const teamOne = document.getElementById("teamOne")
+const teamTwo = document.getElementById("teamTwo")
+const launchGame = document.getElementById("launchGame")
+launchGame.addEventListener("click", ()=>{
+  
+})
